83dbe85faef58fe219d5d3b912aaa570f1aa7af5
Elvince Oduor <124202145+RealElvince@users.noreply.github.com>
2025-12-11 08:27:09 +0300

    data frame in r

---
+++ app.r
@@ -0,0 +1,46 @@
+students <- data.frame(
+  studentid = 1:20,
+  
+  firstname = c(
+    "John","Mary","Kevin","Sarah","Peter",
+    "Aisha","Brian","Linet","James","Faith",
+    "David","Rose","Samuel","Diana","George",
+    "Eva","Dennis","Irene","Felix","Naomi"
+  ),
+  
+  lastname = c(
+    "Kamau","Wanjiku","Otieno","Achieng","Njoroge",
+    "Mohamed","Oduor","Mutheu","Mwangi","Chebet",
+    "Kiptoo","Atieno","Wekesa","Wambui","Kariuki",
+    "Cherono","Kimani","Naliaka","Barasa","Njeri"
+  ),
+  
+  phone = c(
+    "0712345678","0723345678","0700123456","0798123456","0711456789",
+    "0745567890","0799456123","0703456789","0712567890","0722456789",
+    "0798345678","0734123456","0789456123","0719987654","0705678123",
+    "0723678901","0746789012","0791234567","0714789023","0789123456"
+  ),
+  
+  email = c(
+    "john.kamau@students.ku.ac.ke","mary.wanjiku@students.ku.ac.ke",
+    "kevin.otieno@students.ku.ac.ke","sarah.achieng@students.ku.ac.ke",
+    "peter.njoroge@students.ku.ac.ke","aisha.mohamed@students.ku.ac.ke",
+    "brian.oduor@students.ku.ac.ke","linet.mutheu@students.ku.ac.ke",
+    "james.mwangi@students.ku.ac.ke","faith.chebet@students.ku.ac.ke",
+    "david.kiptoo@students.ku.ac.ke","rose.atieno@students.ku.ac.ke",
+    "samuel.wekesa@students.ku.ac.ke","diana.wambui@students.ku.ac.ke",
+    "george.kariuki@students.ku.ac.ke","eva.cherono@students.ku.ac.ke",
+    "dennis.kimani@students.ku.ac.ke","irene.naliaka@students.ku.ac.ke",
+    "felix.barasa@students.ku.ac.ke","naomi.njeri@students.ku.ac.ke"
+  ),
+  
+  marks = c(
+    78, 85, 67, 90, 74,
+    82, 56, 88, 79, 91,
+    65, 73, 84, 92, 69,
+    87, 77, 62, 80, 95
+  )
+)
+
+students
